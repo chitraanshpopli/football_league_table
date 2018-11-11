4d59e86cbaa2628c38b2f1aee12bf21bd56cdeb4
nu_popli <popli.chitraansh@gmail.com>
2018-11-11 15:57:11 +0530

    Added more leagues.

--- football_league_table.py
+++ football_league_table.py
@@ -1,9 +1,50 @@
+#global URLs
+
 #Fetches and displays La Liga table from https://www.bbc.com
 
 import requests
 import pandas as pd
 
-URL = 'https://www.bbc.com/sport/football/spanish-la-liga/table'
+league_numbers = {
+1 : 'LaLiga',
+2 : 'EPL',
+3 : 'Bundesliga',
+4 : 'SerieA',
+5 : 'Ligue1'
+}
+
+URLs = {
+'LaLiga' : 'https://www.bbc.com/sport/football/spanish-la-liga/table',
+'EPL' : 'https://www.bbc.com/sport/football/premier-league/table',
+'Bundesliga' : 'https://www.bbc.com/sport/football/german-bundesliga/table',
+'SerieA' : 'https://www.bbc.com/sport/football/italian-serie-a/table',
+'Ligue1' : 'https://www.bbc.com/sport/football/french-ligue-one/table'
+}
+def set_number_of_teams(league_URL):
+	global rows
+	rows = 20	#Defines the total number of rows.
+	if league_URL == URLs['Bundesliga']:	
+		rows -= 2	#Bundesliga has 18 teams. Other leagues have 20.
+	
+def get_URL():
+	for number, name in league_numbers.items():
+		print(str(number) + '. ' + name)
+	no_league = True
+	while no_league:
+		try:
+			league_number = int(input('Enter the serial number of league you want to see: '))
+		except ValueError:
+			print('\nWrong input.\n')
+			continue
+
+		try:
+			print('\nLoading table for ' + league_numbers[league_number])
+			no_league = False
+		except KeyError:
+			print('\nWrong input.\n')
+			continue
+	set_number_of_teams(URLs[league_numbers[league_number]])
+	return URLs[league_numbers[league_number]]
 
 def set_dataframe_options():
 	#Options for dataframe display.
@@ -21,9 +62,9 @@ def clean_table(df):
 	df = df.fillna('')
 	
 	global last_updated
-	last_updated = df.iat[20,0]
+	last_updated = df.iat[rows,0]
 	
-	df = df.drop([20])
+	df = df.drop([rows])
 	df = df.dropna()	#Removes blank (NaN) values
 	df = df.rename(columns = {'Unnamed: 0' : 'Position', 'Unnamed: 1' : 'Change'})
 	df = clean_position_change(df)
@@ -31,7 +72,7 @@ def clean_table(df):
 
 def clean_position_change(df):
 	#Replaces position change text with symbols for cleaner look.
-	for column in range(0,20):
+	for column in range(rows):
 		if df.iat[column,1] == "team hasn't moved":
 			df.iat[column,1] = 'x'
 		elif df.iat[column,1] == "team has moved up":
@@ -43,8 +84,10 @@ def clean_position_change(df):
 def main():
 	set_dataframe_options()
 	
-	req = requests.get(URL).text
+	league_URL = get_URL()
+	req = requests.get(league_URL).text
 	pd_html = pd.read_html(req)[0]
+	
 	df = pd.DataFrame(pd_html)	#Converts html table to pandas dataframe
 	df = clean_table(df)
 
@@ -52,3 +95,4 @@ def main():
 	print(last_updated)
 	qv = input('Press enter to exit.')
 main()
+
